673520a0a116d567a7a7c8676d37dd5db8b679e1
beastofrock <45082804+beastofrock@users.noreply.github.com>
2021-08-02 11:29:41 +0200

    Update setup_debian.sh

--- for_win_remote/setup_debian.sh
+++ for_win_remote/setup_debian.sh
@@ -2,11 +2,11 @@ apt update
 apt upgrade
 echo"
 first 
-apt install git
+apt install sudo git
 "
 cd ~
 git clone https://github.com/beastofrock/ansible
 
-apt install ansible libkrb5-dev
+apt install ansible libkrb5-dev krb5-user
 echo'
 
