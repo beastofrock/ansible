8e1b86696d91fa88d6decb13a8d74290ac4c7208
beastofrock <45082804+beastofrock@users.noreply.github.com>
2021-08-02 11:41:46 +0200

    Update setup_debian.sh

--- for_win_remote/setup_debian.sh
+++ for_win_remote/setup_debian.sh
@@ -7,7 +7,11 @@ apt install sudo git
 cd ~
 git clone https://github.com/beastofrock/ansible
 
-apt install ansible libkrb5-dev krb5-user
+apt install ansible libkrb5-dev krb5-user python3-pip
 
 sudo pip3 install pywinrm
 
+#nicht bekannte/falsche Domains/realms löschen
+
+sudo nano /etc/krb5.conf
+  
